b11fab0268b74548cde5e54720c4dcb5ca234db0
bberendsen <info@bryanberendsen.nl>
2026-01-16 21:12:41 +0100

    Initial commit: Angular frontend and Laravel backend

---
+++ lw-frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bryanberendsen.lastworkout',
+  appName: 'Last Workout app',
+  webDir: 'dist/lw-frontend/browser'
+};
+
+export default config;
